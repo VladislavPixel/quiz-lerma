9a33be18e6443f29581153be9a55b2c9376862a2
CodeshakeRus <codeshake-rus@yandex.ru>
2023-10-31 23:09:01 +0300

    first commit project

---
+++ src/app/app.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./scss/style.scss";
+import { Routes, Route } from "react-router-dom";
+
+const App = () => {
+	return (
+		<div className='wrapper'>
+			<div className='wrapper__content'>
+				{/* <Routes>
+
+				</Routes> */}
+			</div>
+		</div>
+	);
+}
+
+export { App };
---
+++ src/app/layots/home.tsx
@@ -0,0 +1,9 @@
+import { HomePage } from "../components/pages/home-page";
+
+const Home = () => {
+	return (
+		<HomePage />
+	);
+};
+
+export { Home };
