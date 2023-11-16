080c61f824bdaec214079cc488d6982ee5bcc0c9
VladislavPixel <pixel.world7@mail.ru>
2023-11-16 17:26:15 +0300

    create text-field for form

--- src/app/components/common/form/text-field.tsx
+++ src/app/components/common/form/text-field.tsx
@@ -1,12 +1,51 @@
+import { useState } from "react";
+
 interface ITextField {
 	placeholder: string;
 	name: string;
 	label: string;
+	classesParent: string;
+	type: string;
+	onChange: () => any;
+	iconName?: string;
 };
 
-const TextField = ({ placeholder, name, label }: ITextField) => {
+const TextField = ({ placeholder, name, label, classesParent, type, iconName }: ITextField) => {
+	const [isShow, setShow] = useState<boolean>(false);
+
+	const handlerChange = (event: any) => {
+		console.log(event, "EVENT");
+	};
+
+	const handlerClickIconShow = () => {
+		setShow((prevState) => !prevState);
+	};
+
+	const isIconName = typeof iconName === 'string' && iconName !== '';
+
+	const isPassword = type === 'password';
+
+	const currentIconEyeName = !isShow ? 'eye' : 'eye-secret';
+
+	const correctType = isPassword && isShow ? "text" : isPassword && !isShow ? "password" : type;
+
 	return (
-		<div></div>
+		<div className={`${classesParent}__text-field field-text-block`}>
+			<div className='field-text-block__container'>
+				<label title="Нажмите, чтобы установить указатель на элемент." htmlFor={name} className='field-text-block__label'>{label}</label>
+				<div className='field-text-block__input-wrapper'>
+					<input title='Поле ввода.' className='field-text-block__field' onChange={handlerChange} type={correctType} id={name} name={name} placeholder={placeholder} />
+					{isIconName &&
+						<img src={`./assets/images/icons/${iconName}`} alt={`Иконка поля - ${label}.`} className='field-text-block__icon' />
+					}
+					{isPassword &&
+						<div onClick={handlerClickIconShow} title={`Нажмите, чтобы ${isShow ? 'скрыть' : 'открыть'} пароль.`} className='field-text-block__eye-wrap'>
+							<img src={`./assets/images/icons/${currentIconEyeName}.svg`} className='field-text-block__eye-password' alt="Иконка глазика пароля, которая говорит пользователю скрыт пароль на экране или открыт." />
+						</div>
+					}
+				</div>
+			</div>
+		</div>
 	);
 };
 
--- src/app/components/pages/sign-in-page.tsx
+++ src/app/components/pages/sign-in-page.tsx
@@ -8,25 +8,34 @@ import { TextField } from '../common/form/text-field';
 import { FormComponent } from '../common/form/form-component';
 
 const SignInPage = () => {
-	const [dataForm, setDataForm] = useState({
+	const [dataForm] = useState({
 		login: '',
 		password: ''
 	});
 
-  return (
+	const handlerChangeField = () => {
+
+	};
+
+	return (
 		<Skeleton isSidebar={true} classesParent='sign-in'>
 			<React.Fragment>
 				<LeftColumnAuth {...auxiliaryData.signInPage} classesParent='sign-in' />
 				<RightColumnAuth navigationIter={navigationSignInPage} classesParent='sign-in'>
-					<FormComponent data={dataForm} classesParent='sign-in'>
+					{/* <FormComponent data={dataForm} classesParent='sign-in'>
 						<TextField placeholder='Укажите *логин*' name='login' label='Логин:' />
 						<TextField placeholder='Укажите *пароль*' name='password' label='Пароль:' />
 						<button>Вход</button>
-					</FormComponent>
+					</FormComponent> */}
+					<form className='sign-in__form'>
+						<TextField iconName='login.svg' type='text' onChange={handlerChangeField} classesParent='sign-in' placeholder='Укажите *логин*' name='login' label='Логин:' />
+						<TextField iconName='password.svg' type='password' onChange={handlerChangeField} classesParent='sign-in' placeholder='Укажите *пароль*' name='password' label='Пароль:' />
+						<button title='Нажмите, чтобы отправить данные на авторизацию.' className='sign-in__sub-btn btn button-purple'>Вход</button>
+					</form>
 				</RightColumnAuth>
 			</React.Fragment>
 		</Skeleton>
-  );
+	);
 };
 
 export { SignInPage };
